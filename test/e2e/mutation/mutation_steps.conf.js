56b0d2b67e12151821e667997063f13eccf6e330
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2018-12-07 13:48:09 +0000

    Cleaning up Stryker config and adding step tests

--- test/e2e/mutation/mutation_steps.conf.js
+++ test/e2e/mutation/mutation_steps.conf.js
@@ -13,15 +13,33 @@ const strykerConfiguration = config => {
     coverageAnalysis: 'perTest',
     mutate:
       [
-        'steps/check-your-appeal/*.js',
-        '!test/unit/steps/check-your-appeal/*.test.js'
+        'steps/**/*.js'
+        // 'steps/check-your-appeal/*.js',
+        // '!test/unit/steps/check-your-appeal/*.test.js'
       ],
-    files: ['**/*'],
+    // files: ['**/*'],
     maxConcurrentTestRunners: 2,
     symlinkNodeModules: false,
     htmlReporter: { baseDir: 'functional-output/mutation-steps' },
     mochaOptions: {
-      files: ['test/unit/steps/check-your-appeal/*.test.js'],
+      files: [
+        'test/unit/steps/check-your-appeal/**/*.test.js',
+        'test/unit/steps/compliance/**/*.test.js',
+        'test/unit/steps/check-your-appeal/**/*.test.js',
+        'test/unit/steps/compliance/**/*.test.js',
+        'test/unit/steps/confirmation/**/*.test.js',
+        'test/unit/steps/entry/**/*.test.js',
+        'test/unit/steps/errors/**/*.test.js',
+        'test/unit/steps/exit/**/*.test.js',
+        'test/unit/steps/hearing/**/*.test.js',
+        'test/unit/steps/identity/**/*.test.js',
+        'test/unit/steps/reasons-for-appealing/**/*.test.js',
+        'test/unit/steps/representative/**/*.test.js',
+        'test/unit/steps/session/**/*.test.js',
+        'test/unit/steps/sms-notify/**/*.test.js'
+        // 'test/unit/steps/start/*.test.js'
+        // 'test/assets/**/*.test.js'
+      ],
       timeout: 8000
     },
     logLevel: 'debug',
