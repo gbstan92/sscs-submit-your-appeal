93145e21e5de4b5940b9e7be5d955a421629d9fb
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2019-01-21 08:43:14 +0000

    Phone Validation on Appellant contacts

--- steps/identity/appellant-contact-details/AppellantContactDetails.js
+++ steps/identity/appellant-contact-details/AppellantContactDetails.js
@@ -1,8 +1,9 @@
+const { parsePhoneNumberFromString } = require('libphonenumber-js');
 const { Question, goTo } = require('@hmcts/one-per-page');
 const { form, text } = require('@hmcts/one-per-page/forms');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const { get } = require('lodash');
-const { postCode, whitelist, phoneNumber } = require('utils/regex');
+const { postCode, whitelist } = require('utils/regex');
 const { Logger } = require('@hmcts/nodejs-logging');
 const sections = require('steps/check-your-appeal/sections');
 const Joi = require('joi');
@@ -28,6 +29,22 @@ const customJoi = Joi.extend(joi => {
         validate(params, value, state, options) {
           if (params.invalidPostcode) {
             return this.createError('string.validatePostcode', { v: value }, state, options);
+          }
+
+          return value;
+        }
+      },
+      {
+        name: 'validatePhone',
+        validate(params, value, state, options) {
+          const parsedPhoneNumber = parsePhoneNumberFromString(value, 'GB');
+          let isValidPhone = false;
+          if (parsedPhoneNumber && parsedPhoneNumber.isValid) {
+            isValidPhone = parsedPhoneNumber.isValid();
+          }
+
+          if (!isValidPhone) {
+            return this.createError('string.validatePhone', { v: value }, state, options);
           }
 
           return value;
@@ -100,7 +117,8 @@ class AppellantContactDetails extends Question {
       ),
       phoneNumber: text.joi(
         fields.phoneNumber.error[prefix].invalid,
-        Joi.string().regex(phoneNumber).allow('')
+        // Joi.string().regex(phoneNumber).allow('')
+        customJoi.string().trim().validatePhone()
       ),
       emailAddress: text.joi(
         fields.emailAddress.error[prefix].invalid,
